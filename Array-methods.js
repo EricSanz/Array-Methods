23da802143fca1fdd400e4b4bd3ee894e52f68fc
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2021-04-15 16:22:04 +0200

    push method done and tested

--- Array-methods.js
+++ Array-methods.js
@@ -1,4 +1,14 @@
-let SkylabArray = {
+let skylabArray = {
+
+    push: (fn, object) => {
+        return {
+            ...fn,
+            [fn.length]: object,
+            length: fn.length + 1,
+            __proto__: skylabArray
+        };
+    },
+
     map: (fn, object) => {
         let newObject = {};
         for (let property in object) {
@@ -16,7 +26,7 @@ let testArray = {
     1: 2,
     2: 3,
     length: 3,
-    __proto__: SkylabArray
+    __proto__: skylabArray
 };
 
-module.exports = { testArray, SkylabArray };
+module.exports = { testArray, skylabArray };
--- Array-methods.test.js
+++ Array-methods.test.js
@@ -1,22 +1,45 @@
 const { test, expect } = require('@jest/globals');
-const { testArray } = require('./Array-methods');
+const { testArray, skylabArray } = require('./Array-methods');
 
 describe('Array Methods', () => {
 
-    test('testArray length should return 3', () => {
-        //arrange
-        //act
-        const response = testArray.length;
-        //assert
-        expect(response).toBe(3);
-    });
+    describe('Push method', () => {
+        
+        test('Should add an element to the end of the array', () => {
+            //arrange
+            //act
+            const response = skylabArray.push(testArray, 'newElement');
+            //assert
+            expect(response[3]).toBe('newElement');
+        });
 
-    test('map on testArray should return double value on every index', () => {
-        //arrange
-        //act
-        const response = testArray.map((x) => x * 2, testArray);
-        //assert
-        expect(response).toEqual({ '0': 2, '1': 4, '2': 6, length: 3});
-    });
+        test('The length of testArray should be 4 after adding one element', () => {
+            //arrange
+            //act
+            const response = skylabArray.push(testArray, 'newElement');
+            //assert
+            expect(response.length).toBe(4);
+        });
+    })
+
+    describe('Map method', () => {
+
+        test('testArray length should return 3', () => {
+            //arrange
+            //act
+            const response = testArray.length;
+            //assert
+            expect(response).toBe(3);
+        });
+    
+        test('map on testArray should return double value on every index', () => {
+            //arrange
+            //act
+            const response = skylabArray.map((x) => x * 2, testArray);
+            //assert
+            expect(response).toEqual({ '0': 2, '1': 4, '2': 6, length: 3});
+        });
+
+    })
 
 });
