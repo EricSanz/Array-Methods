ce4eed17a7842491671ea8a324fee73f9ffc527c
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2021-04-15 17:11:49 +0200

    added fill method with test and comments about every method

--- Array-methods.js
+++ Array-methods.js
@@ -1,5 +1,6 @@
 let skylabArray = {
 
+    // The push() method adds one or more elements to the end of an array and return the new length of the array.
     push: (fn, object) => {
         return {
             ...fn,
@@ -9,6 +10,7 @@ let skylabArray = {
         };
     },
 
+    // The map() method creates a new array populated with the results of calling a provided function on every element in the calling array.
     map: (fn, object) => {
         let newObject = {};
         for (let property in object) {
@@ -18,7 +20,21 @@ let skylabArray = {
         }
         newObject.length = object.length;
         return newObject;
-    }
+    },
+
+    // The fill() method changes all elements in an array to a static value, from a start index(default 0) to an end index(default array.length). it returns the modified array.
+    fill: (object, value, start = 0, end = object.length) => {
+        let newObject = {};
+        newObject.length = object.length;
+        for (let property in object) {
+            if ((property < start || property >= end) && property !== 'length' && object.hasOwnProperty(property)) {
+                newObject[property] = object[property];
+            } else if (property >= start && property < end && property !== 'length' && object.hasOwnProperty(property)) {
+                newObject[property] = value;
+            }
+        };
+        return newObject;
+    } 
 }
 
 let testArray = {
--- Array-methods.test.js
+++ Array-methods.test.js
@@ -1,5 +1,5 @@
-const { test, expect } = require('@jest/globals');
-const { testArray, skylabArray } = require('./Array-methods');
+const { test, expect, describe } = require('@jest/globals');
+const { testArray } = require('./Array-methods');
 
 describe('Array Methods', () => {
 
@@ -8,7 +8,7 @@ describe('Array Methods', () => {
         test('Should add an element to the end of the array', () => {
             //arrange
             //act
-            const response = skylabArray.push(testArray, 'newElement');
+            const response = testArray.push(testArray, 'newElement');
             //assert
             expect(response[3]).toBe('newElement');
         });
@@ -16,7 +16,7 @@ describe('Array Methods', () => {
         test('The length of testArray should be 4 after adding one element', () => {
             //arrange
             //act
-            const response = skylabArray.push(testArray, 'newElement');
+            const response = testArray.push(testArray, 'newElement');
             //assert
             expect(response.length).toBe(4);
         });
@@ -35,11 +35,46 @@ describe('Array Methods', () => {
         test('map on testArray should return double value on every index', () => {
             //arrange
             //act
-            const response = skylabArray.map((x) => x * 2, testArray);
+            const response = testArray.map((x) => x * 2, testArray);
             //assert
             expect(response).toEqual({ '0': 2, '1': 4, '2': 6, length: 3});
         });
 
+    });
+
+    describe('Fill method', () => {
+
+        test('Should change to 10 in every index', () => {
+            //arrange
+            //act
+            const response = testArray.fill(testArray, 10);
+            //assert
+            expect(response).toEqual({ '0': 10, '1': 10, '2': 10, length: 3});
+        });
+
+        test('Should change to 10 in index 1 and 2', () => {
+            //arrange
+            //act
+            const response = testArray.fill(testArray, 10,1);
+            //assert
+            expect(response).toEqual({ '0': 1, '1': 10, '2': 10, length: 3});
+        });
+
+        test('Should change to 10 in index 0', () => {
+            //arrange
+            //act
+            const response = testArray.fill(testArray, 10,0,1);
+            //assert
+            expect(response).toEqual({ '0': 10, '1': 2, '2': 3, length: 3});
+        });
+
+        test('Should change to 100 in last index', () => {
+            //arrange
+            //act
+            const response = testArray.fill(testArray, 100, testArray.length -1)
+            //assert
+            expect(response).toEqual({ '0': 1, '1': 2, '2': 100, length: 3});
+        });
     })
 
 });
