65e2ee6587135136a325485f3c6252090b63c978
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2021-04-16 13:12:32 +0200

    added filter method tested

--- Array-methods.js
+++ Array-methods.js
@@ -77,6 +77,19 @@ let skylabArray = {
             }
         }
         return -1;
+    },
+
+    // The filter() method creates a new array with all elements that pass the test implemented by the provided function.
+    filter: (fn, object) => {
+        let newObject = {};
+        newObject.length = 0;
+        for (let property in object) {
+            if (fn(object[property]) && property !== 'length' && object.hasOwnProperty(property)) {
+                newObject[property] = object[property];
+                newObject.length++;
+            }
+        }
+        return newObject;
     }
 }
 
--- Array-methods.test.js
+++ Array-methods.test.js
@@ -157,6 +157,35 @@ describe('Array Methods', () => {
             //assert
             expect(response).toBe(-1);
         })
+    });
+
+    describe('filter method', () => {
+        test('Should return index 1 and 2', () => {
+            //arrange
+            const array = { '0': 1, '1': 45, '2': 20, '3': -1, '4': -20, length: 5};
+            //act
+            const response = skylabArray.filter((x) => x > 1, array);
+            //assert
+            expect(response).toEqual({ '1': 45, '2': 20, length: 2 });
+        });
+
+        test('Should return only numbers 4', () => {
+            //arrange
+            const array = { '0': 1, '1': 4, '2': 20, '3': -1, '4': 4, length: 5};
+            //act
+            const response = skylabArray.filter((x) => x === 4, array);
+            //assert
+            expect(response).toEqual({ '1': 4, '4': 4, length: 2 })
+        });
+
+        test('Should return only strings', () => {
+            //arrange
+            const array = { '0': '1', '1': 45, '2': 'twenty', '3': -1, '4': -20, length: 5};
+            //act
+            const response = skylabArray.filter((x) => typeof x === 'string', array);
+            //assert
+            expect(response).toEqual( {'0': '1', '2': 'twenty', length: 2});
+        })
     })
 
 });
