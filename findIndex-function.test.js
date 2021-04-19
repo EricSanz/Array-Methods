9827fe99695555bac8b47bb53d60e86b05788c1e
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2021-04-19 12:33:31 +0200

    added beforeEach in tests

--- findIndex-function.test.js
+++ findIndex-function.test.js
@@ -1,9 +1,12 @@
 const findIndexFunction = require('./findIndex-function');
 
 describe('FindIndex method', () => {
+
+    beforeEach(() => {
+        testArray = { '0': 1, '1': 45, '2': 20, '3': -1, '4': -20, length: 5};
+    })
     test('Should return the index of the first number greater than 25, in this case, index 1', () => {
         //arrange
-        const testArray = { '0': 1, '1': 45, '2': 20, '3': -1, '4': -20, length: 5};
         //act
         const response = findIndexFunction((x) => x > 25, testArray);
         //assert
@@ -12,7 +15,6 @@ describe('FindIndex method', () => {
 
     test('Should return the index of the first number below -5, in this case, index 4', () => {
         //arrange
-        const testArray = { '0': 1, '1': 45, '2': 20, '3': -1, '4': -20, length: 5};
         //act
         const response = findIndexFunction((x) => x < -5, testArray);
         //assert
@@ -21,7 +23,6 @@ describe('FindIndex method', () => {
 
     test('Should return -1', () => {
         //arrange
-        const testArray = { '0': 1, '1': 45, '2': 20, '3': -1, '4': -20, length: 5};
         //act
         const response = findIndexFunction((x) => x === 0, testArray);
         //assert
