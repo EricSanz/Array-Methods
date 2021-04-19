9de2d90840a2aa8b0bfe088626ff40cccc8a2a7b
Eric Sanz <eric_sanz_rodriguez@hotmail.com>
2021-04-19 11:44:17 +0200

    added reduce method tested

--- Array-methods.js
+++ Array-methods.js
@@ -114,6 +114,25 @@ let skylabArray = {
             }
         }
         return true;
+    },
+
+    // The reduce() methodexecutes a reducer function (that you provide) on each element of the array, resulting in single output value.
+
+    reduce: (fn, object, initialValue) => {
+
+        let iterator = 0;
+        let accumulator = initialValue;
+
+        if (initialValue === undefined) {
+            accumulator = object[0];
+            iterator++;
+        }
+
+        for (iterator; iterator < object.length; iterator++) {
+            accumulator = fn(accumulator, object[iterator]);
+        }
+
+        return accumulator;
     }
 }
 
